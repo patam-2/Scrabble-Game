57e3e58fb61b795c1136c1a717753141620b6893
ArielleBenOvadia <130040109+ArielleBenOvadia@users.noreply.github.com>
2023-06-04 16:14:41 +0300

    Add files via upload

---
+++ View/View/ViewController.java
@@ -0,0 +1,12 @@
+package View;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ViewController implements Observer
+{
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
