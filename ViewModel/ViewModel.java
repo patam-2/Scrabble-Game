1dd42bad6c43f544583e66f9ba7fc511dbc0837a
Arielle Ben Ovadia <130040109+ArielleBenOvadia@users.noreply.github.com>
2023-06-13 16:28:42 +0300

    Add files via upload

--- ViewModel/ViewModel.java
+++ ViewModel/ViewModel.java
@@ -1,6 +1,51 @@
 package ViewModel;
 
-public class ViewModel
-{
+import Model.GameManager;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import java.util.Observable;
+import java.util.Observer;
 
+
+public class ViewModel extends Observable implements Observer {
+
+    public GameManager gameManager;
+    public SimpleIntegerProperty rounds;
+    public SimpleStringProperty hostPort;
+    public SimpleStringProperty numOfClients;
+
+    public ViewModel(GameManager gameManagerModel) {
+        this.numOfClients = new SimpleStringProperty();
+        this.hostPort = new SimpleStringProperty();
+        this.rounds = new SimpleIntegerProperty();
+        this.gameManager = gameManagerModel;
+        this.gameManager.addObserver(this);
+    }
+
+
+    @Override
+    public void update(Observable o, Object arg)
+    {
+        this.numOfClients = new SimpleStringProperty(String.valueOf(this.gameManager.host.numberOfClients));
+        this.rounds = new SimpleIntegerProperty(this.gameManager.host.rounds);
+        System.out.println("num of clients: " + gameManager.host.numberOfClients);
+        setChanged();
+        notifyObservers();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @FXML
+//    public ObservableMap<String, List<SimpleObjectProperty<Tile>>> map = FXCollections.observableHashMap();
